dc77281b0b98c6ba4296513104b61d010e9f9925
ThisIsBrady <30638248+ThisIsBrady@users.noreply.github.com>
2023-05-09 22:23:19 -0500

    Add code to toggle LEDs

--- CCD_Software/CCD_Software.h
+++ CCD_Software/CCD_Software.h
@@ -9,7 +9,6 @@
 EthernetServer TCPServer(RC_ROVECOMM_ETHERNET_TCP_PORT);
 RoveCommEthernet RoveComm;
 
-
 // Fluorometer Pins
 #define CCD_IN A10
 #define SH 2
@@ -19,6 +18,11 @@ RoveCommEthernet RoveComm;
 #define DATA_CLK 5
 #define DATA_CLK_IN 8
 
+#define CONTROL_LED_265 27
+#define CONTROL_LED_275 38
+#define CONTROL_LED_280 39
+#define CONTROL_LED_310 40
+#define CONTROL_LED_365 41
 
 // Variables
 uint16_t ccd_buff[3648];
--- CCD_Software/CCD_Software.ino
+++ CCD_Software/CCD_Software.ino
@@ -1,15 +1,21 @@
-#include "CCD_Software.h";
+#include "CCD_Software.h"
 
-
-void setup() {
+void setup()
+{
     Serial.begin(9600);
 
     pinMode(MASTER_CLK, OUTPUT);
     pinMode(CLK_IN, INPUT);
 
+    pinMode(CONTROL_LED_265, OUTPUT);
+    pinMode(CONTROL_LED_275, OUTPUT);
+    pinMode(CONTROL_LED_280, OUTPUT);
+    pinMode(CONTROL_LED_310, OUTPUT);
+    pinMode(CONTROL_LED_365, OUTPUT);
+
     analogWriteResolution(8);
     analogWriteFrequency(MASTER_CLK, 1000000);
-    analogWriteFrequency(DATA_CLK, 1000000/4);
+    analogWriteFrequency(DATA_CLK, 1000000 / 4);
     analogWrite(MASTER_CLK, 128);
     analogWrite(DATA_CLK, 32);
 
@@ -18,66 +24,74 @@ void setup() {
     Serial.println("Complete");
 }
 
-void loop() {
+void loop()
+{
     rovecomm_packet packet = RoveComm.read();
-    switch (packet.data_id) {
+    switch (packet.data_id)
+    {
 
-        // Turn on/off LEDs
-        case RC_SCIENCESENSORSBOARD_FLUOROMETERLEDS_DATA_ID:
-        {
-            uint8_t data = ((uint8_t*) packet.data)[0];
-        }
-
-        // Read and transmit CCD buffer
-        case RC_SCIENCESENSORSBOARD_REQFLUOROMETER_DATA_ID:
-        {
-            Serial.println("Reading...");
-            readCCD();
-
-            Serial.println("Writing...");
-            RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA1_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA1_DATA_COUNT, (&ccd_buff[0]));
-            RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA2_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA2_DATA_COUNT, (&ccd_buff[500]));
-            RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA3_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA3_DATA_COUNT, (&ccd_buff[1000]));
-            RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA4_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA4_DATA_COUNT, (&ccd_buff[1500]));
-            RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA5_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA5_DATA_COUNT, (&ccd_buff[2000]));
-            RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA6_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA6_DATA_COUNT, (&ccd_buff[2500]));
-            RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA7_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA7_DATA_COUNT, (&ccd_buff[3000]));
-            RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA8_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA8_DATA_COUNT, (&ccd_buff[3500]));
-
-            break;
-        }
-
-        // Default
-        default:
-        {
-            break;
-        }
+    // Turn on/off LEDs
+    case RC_SCIENCESENSORSBOARD_FLUOROMETERLEDS_DATA_ID:
+    {
+        uint8_t data = ((uint8_t *)packet.data)[0];
+        digitalWrite(CONTROL_LED_265, data & (1 << 0) ? HIGH : LOW);
+        digitalWrite(CONTROL_LED_275, data & (1 << 1) ? HIGH : LOW);
+        digitalWrite(CONTROL_LED_280, data & (1 << 2) ? HIGH : LOW);
+        digitalWrite(CONTROL_LED_310, data & (1 << 3) ? HIGH : LOW);
+        digitalWrite(CONTROL_LED_365, data & (1 << 4) ? HIGH : LOW);
+        break;
     }
 
-    
+    // Read and transmit CCD buffer
+    case RC_SCIENCESENSORSBOARD_REQFLUOROMETER_DATA_ID:
+    {
+        Serial.println("Reading...");
+        readCCD();
+
+        Serial.println("Writing...");
+        RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA1_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA1_DATA_COUNT, (&ccd_buff[0]));
+        RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA2_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA2_DATA_COUNT, (&ccd_buff[500]));
+        RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA3_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA3_DATA_COUNT, (&ccd_buff[1000]));
+        RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA4_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA4_DATA_COUNT, (&ccd_buff[1500]));
+        RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA5_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA5_DATA_COUNT, (&ccd_buff[2000]));
+        RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA6_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA6_DATA_COUNT, (&ccd_buff[2500]));
+        RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA7_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA7_DATA_COUNT, (&ccd_buff[3000]));
+        RoveComm.write(RC_SCIENCESENSORSBOARD_FLUOROMETERDATA8_DATA_ID, RC_SCIENCESENSORSBOARD_FLUOROMETERDATA8_DATA_COUNT, (&ccd_buff[3500]));
+
+        break;
+    }
+
+    // Default
+    default:
+    {
+        break;
+    }
+    }
 }
 
+void readCCD()
+{
+    while (!digitalRead(DATA_CLK_IN))
+        ;
 
+    digitalWrite(SH, LOW);
+    digitalWrite(ICG, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(SH, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(ICG, LOW);
 
-void readCCD() {
-  while (!digitalRead(DATA_CLK_IN));
-  
-  digitalWrite(SH, LOW);
-  digitalWrite(ICG, HIGH);
-  delayMicroseconds(2);
-  digitalWrite(SH, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(ICG, LOW);
-
-  ccd_buff_index = 0;
-  attachInterrupt(digitalPinToInterrupt(DATA_CLK_IN), ccd_isr, RISING);
-  delay(20);
-  detachInterrupt(digitalPinToInterrupt(DATA_CLK_IN));
+    ccd_buff_index = 0;
+    attachInterrupt(digitalPinToInterrupt(DATA_CLK_IN), ccd_isr, RISING);
+    delay(20);
+    detachInterrupt(digitalPinToInterrupt(DATA_CLK_IN));
 }
 
-void ccd_isr() {
-    if (ccd_buff_index >= 32 && ccd_buff_index < (32+3648)) {
-        ccd_buff[ccd_buff_index-32] = analogRead(CCD_IN);
+void ccd_isr()
+{
+    if (ccd_buff_index >= 32 && ccd_buff_index < (32 + 3648))
+    {
+        ccd_buff[ccd_buff_index - 32] = analogRead(CCD_IN);
     }
     ccd_buff_index++;
 }
