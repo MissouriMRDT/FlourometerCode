7f50fc4cec5479068b60396ed7678ba7efb53b73
Malacki Ehlers <ehlers.malacki@gmail.com>
2024-04-23 21:37:06 -0500

    Add variable integration time

--- Fluorometer_Software/Fluorometer_Software.ino
+++ Fluorometer_Software/Fluorometer_Software.ino
@@ -3,9 +3,10 @@
 void setup() {
     // Serial Debugger
     Serial.begin(115200);
-    Serial.println("");
 
-    RoveComm.begin(RC_FLUOROMETERBOARD_FIRSTOCTET, RC_FLUOROMETERBOARD_SECONDOCTET, RC_FLUOROMETERBOARD_THIRDOCTET, RC_FLUOROMETERBOARD_FOURTHOCTET, &TCPServer);
+    Serial.println("Reflectance setup...");
+    RoveComm.begin(RC_REFLECTANCESPECTROMETERBOARD_FIRSTOCTET, RC_REFLECTANCESPECTROMETERBOARD_SECONDOCTET, RC_REFLECTANCESPECTROMETERBOARD_THIRDOCTET, RC_REFLECTANCESPECTROMETERBOARD_FOURTHOCTET, &TCPServer);
+    Serial.println("Complete");
 
     miniSpec.init();
 }
@@ -14,10 +15,13 @@ void loop() {
 
   rovecomm_packet packet = RoveComm.read();
   switch (packet.data_id) {
-    case (RC_FLUOROMETERBOARD_REQUESTREADING_DATA_ID): {
+    case (RC_REFLECTANCESPECTROMETERBOARD_REQUESTREADING_DATA_ID): {
+      uint32_t data = ((uint32_t*) packet.data)[0];
+      miniSpec.setIntegrationTime(data);
+
       uint8_t video[288];
       miniSpec.read(video);
-      RoveComm.write(RC_FLUOROMETERBOARD_READING_DATA_ID, RC_FLUOROMETERBOARD_READING_DATA_COUNT, video);
+      RoveComm.write(RC_REFLECTANCESPECTROMETERBOARD_READING_DATA_ID, RC_REFLECTANCESPECTROMETERBOARD_READING_DATA_COUNT, video);
     }
   }
     
